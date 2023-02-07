831ae2c9cf1ace43f9267070569fa8592702a5be
Andres Duque <analista.desarrollo3@hyundailatinoamerica.com>
2023-02-06 20:41:38 -0500

    punto

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import http from 'http'
-import { routes } from './routes/routes.js'
-import { config } from './config.js'
-import { sequelizeConnection } from './database/db.js'
+import { routes } from './src/routes/routes.js'
+import { config } from './src/config.js'
+import { sequelizeConnection } from './src/database/db.js'
 const app = express()
 app.use(express.json())
 app.use(cors())
