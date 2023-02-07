523ea678fbedeacd694487182a17f890205aec5d
Andres Duque <analista.desarrollo3@hyundailatinoamerica.com>
2023-02-06 21:34:46 -0500

    .2

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,12 @@
 import Sequelize from 'sequelize'
 import { config } from '../config.js'
-
+import mysql2 from 'mysql2'
 const sequelizeConnection = new Sequelize(config.dbName, config.dbUser, config.dbPassword, {
   host: config.dbHost,
   dialect: 'mysql',
   port: config.dbPort,
-  timezone: '-05:00'
+  timezone: '-05:00',
+  dialectModule: mysql2
 })
 
 export {
