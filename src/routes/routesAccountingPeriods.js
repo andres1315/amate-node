40cbfa883b73d3d214fd7be7634539ad1165c498
Andres Duque <andress1315@gmail.com>
2023-04-11 21:58:10 -0500

    feat: commnet

--- src/routes/routesAccountingPeriods.js
+++ src/routes/routesAccountingPeriods.js
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { getAccountingPeriods, createAccountingPeriods, OpenCloseAccountingPeriods } from '../controllers/accountingPeriodsController.js'
 
 export const routesAccountingPeriods = Router()
-
+/* routes */
 routesAccountingPeriods
   .get('/', getAccountingPeriods)
   .post('/create/', createAccountingPeriods)
