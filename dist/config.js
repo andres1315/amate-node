81a17e08cae1f17257b264aa124052cffa35fa2c
Andres Duque <andress1315@gmail.com>
2023-09-07 19:33:58 -0500

    soiveld

--- dist/config.js
+++ dist/config.js
@@ -1,15 +1,15 @@
-"use strict";
+'use strict'
 
-var _typeof = require("@babel/runtime/helpers/typeof");
-Object.defineProperty(exports, "__esModule", {
+const _typeof = require('@babel/runtime/helpers/typeof')
+Object.defineProperty(exports, '__esModule', {
   value: true
-});
-exports.config = void 0;
-var dotenv = _interopRequireWildcard(require("dotenv"));
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { "default": obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj["default"] = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-dotenv.config();
-var config = {
+})
+exports.config = void 0
+const dotenv = _interopRequireWildcard(require('dotenv'))
+function _getRequireWildcardCache (nodeInterop) { if (typeof WeakMap !== 'function') return null; const cacheBabelInterop = new WeakMap(); const cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache (nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop })(nodeInterop) }
+function _interopRequireWildcard (obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj } if (obj === null || _typeof(obj) !== 'object' && typeof obj !== 'function') { return { default: obj } } const cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj) } const newObj = {}; const hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (const key in obj) { if (key !== 'default' && Object.prototype.hasOwnProperty.call(obj, key)) { const desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc) } else { newObj[key] = obj[key] } } } newObj.default = obj; if (cache) { cache.set(obj, newObj) } return newObj }
+dotenv.config()
+const config = {
   port: process.env.PORT || 3000,
   host: process.env.HOST || 'localhost',
   dbUser: process.env.DB_USER_POSTGRES,
@@ -18,5 +18,5 @@ var config = {
   dbName: process.env.DB_NAME_POSTGRES,
   dbPort: process.env.DB_PORT_POSTGRES,
   secret: process.env.SECRET
-};
-exports.config = config;
+}
+exports.config = config
--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,23 @@
-import express from 'express'
-import cors from 'cors'
-import http from 'http'
-import { routes } from './src/routes/routes.js'
-import { config } from './src/config.js'
-import helmet from 'helmet'
-import { sequelizeConnectionPostgres } from './src/database/postgres.js'
+'use strict'
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(helmet())
-
-http.createServer(app).listen(config.port, () => {
-  console.log(`Server is running on port ${config.port}`)
-})
-
-routes(app)
+const _interopRequireDefault = require('@babel/runtime/helpers/interopRequireDefault')
+const _express = _interopRequireDefault(require('express'))
+const _cors = _interopRequireDefault(require('cors'))
+const _http = _interopRequireDefault(require('http'))
+const _routes = require('./routes/routes.js')
+const _config = require('./config.js')
+const _helmet = _interopRequireDefault(require('helmet'))
+const _postgres = require('./database/postgres.js')
+const app = (0, _express.default)()
+app.use(_express.default.json())
+app.use((0, _cors.default)())
+app.use((0, _helmet.default)())
+_http.default.createServer(app).listen(_config.config.port, function () {
+  console.log('Server is running on port '.concat(_config.config.port))
+});
+(0, _routes.routes)(app)
 try {
-  sequelizeConnectionPostgres.authenticate()
+  _postgres.sequelizeConnectionPostgres.authenticate()
   console.log('Connection has been established successfully.')
 } catch (error) {
   console.error('Unable to connect to the database:', error)
